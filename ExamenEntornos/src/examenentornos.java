7e9922d67f917c16a3cd6822f0fdf56e75bac1cc
1-DAM <1-DAM@Aula2-PC01>
2016-03-11 13:48:13 +0100

    1.- primer commit

---
+++ ExamenEntornos/src/examenentornos.java
@@ -0,0 +1,12 @@
+
+public class examenentornos {
+
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        System.out.printf("La suma es de %d + %d es %d",a, b, Sumar(a, b));
+    }
+    public static int Sumar(int a, int b){     
+        return a + b;
+    }
+}
