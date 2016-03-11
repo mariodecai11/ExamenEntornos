d2a2c566f3f4b2a537f71fc8169dc2e41d605c59
mariodecai11 <marioponcegarcia@gmail.com>
2016-03-11 14:04:27 +0100

    3.- Modificación de la variable b.

--- ExamenEntornos/src/examenentornos.java
+++ ExamenEntornos/src/examenentornos.java
@@ -3,7 +3,7 @@ public class examenentornos {
 
     public static void main(String[] args) {
         int a = 4;
-        int b = 7;
+        int b = 32;
         System.out.printf("La suma es de %d + %d es %d",a, b, Sumar(a, b));
     }
     public static byte Sumar(int a, int b){     
